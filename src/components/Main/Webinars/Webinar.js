e51ac0059ba8a44df97b74daa580e8959657f259
kamilakirol <kirolkamila@wp.pl>
2022-12-14 20:57:26 +0100

    fi import warning

--- src/components/Main/Webinars/Webinar.js
+++ src/components/Main/Webinars/Webinar.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 
 const Webinar = ({webinar, openModal, usedTickets}) => {
     const {title, date, text, image, tickets, id} = webinar;
